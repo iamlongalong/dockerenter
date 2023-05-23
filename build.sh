801083e3bff8cc81f283581507ad7187ad64ca9c
long <huhailong@jwzg.com>
2023-05-23 22:51:26 +0800

    add build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/sshw-darwin-amd64 .
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o build/sshw-darwin-arm64 .
+
+CGO_ENABLED=0 GOOS=windows GOARCH=386 go build -o build/sshw-windows-386 .
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/sshw-windows-amd64 .
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/sshw-linux-amd64 .
+CGO_ENABLED=0 GOOS=linux GOARCH=386 go build -o build/sshw-linux-386 .
+CGO_ENABLED=0 GOOS=linux GOARCH=arm go build -o build/sshw-linux-arm .
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o build/sshw-linux-arm64 .
+CGO_ENABLED=0 GOOS=linux GOARCH=mips go build -o build/sshw-linux-mips .
+CGO_ENABLED=0 GOOS=linux GOARCH=mips64 go build -o build/sshw-linux-mips64 .
